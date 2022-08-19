23835a42b1f11c30ce5b0f61d0d6873903c61209
Lucas Rodrigues Rossato <89791115+luks-rossato@users.noreply.github.com>
2022-08-19 17:02:03 -0300

    creating main

--- marketapp/lib/main.dart
+++ marketapp/lib/main.dart
@@ -4,3 +4,16 @@ void main() {
   runApp(Shoplist());
 }
 
+class Shoplist extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Shoplist',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: ListadeCompras(),
+    );
+  }
+}
+
